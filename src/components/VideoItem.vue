7851e044a12beda2ca494f3010a3c8839c0dd27d
mincheol-creator <kimmc1030@naver.com>
2020-06-18 14:30:48 +0900

    .

---
+++ src/components/VideoItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img @click="imageClicked" :src="video.snippet.thumbnails.default.url" />
+    <p>{{ video.snippet.title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoItem",
+  props: {
+    video: Object
+  },
+  methods: {
+    imageClicked() {
+      this.$emit("imageClicked", this.video);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="embed-responsive embed-responsive-16by9" v-if="video">
+    <iframe
+      class="embed-responsive-item"
+      v-bind:src="`https://www.youtube.com/embed/${video.id.videoId}`"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoPlayer",
+  props: {
+    video: Object
+  }
+};
+</script>
+
+<style>
+</style>
